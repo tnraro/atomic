86c376d8c66f959d35876cb7da5c42b599c66bd7
tnraro <deokji53@gmail.com>
2024-07-28 20:26:33 +0900

    leaking

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,10 +1,10 @@
 use std::thread;
 
 fn main() {
-    static X: [i32; 3] = [1, 2, 3];
+    let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
 
-    let t1 = thread::spawn(|| dbg!(&X));
-    let t2 = thread::spawn(|| dbg!(&X));
+    let t1 = thread::spawn(move || dbg!(x));
+    let t2 = thread::spawn(move || dbg!(x));
 
     t1.join().unwrap();
     t2.join().unwrap();
